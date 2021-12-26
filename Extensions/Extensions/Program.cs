66db0ea9573dbfe98aa25a563af18556ed470e3a
VusalAhmed <94452551+VusalAhmed@users.noreply.github.com>
2021-12-27 01:43:28 +0400

    Add files via upload

---
+++ Extensions/Extensions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Extensions
+{
+  public  class Program
+    {
+        static public void  Main(string[] args)
+        {
+            string str1 = "Hello world";
+            string str2 = "Hello world";
+            
+            
+            bool a, b;
+
+            a = str1.EndsWith("world");
+            Console.WriteLine(a.ToString());
+             
+            b = str2.EndsWith("Hello");
+            Console.WriteLine(b.ToString());
+           
+
+
+        }
+    }
+}
